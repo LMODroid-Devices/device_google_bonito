7b3d941294a2df65f3a000b0899e334afe1ff879
Erfan Abdi <erfangplus@gmail.com>
2022-08-23 12:39:18 +0430

    bonito: Rename manifest to lineage
    Change-Id: Ib942e04cab8c8d6f2470624a259aa0820c7d0d14

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -14,7 +14,7 @@ TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
 TARGET_NEEDS_DTBOIMAGE := true
 
 # Manifests
-DEVICE_MANIFEST_FILE += device/google/bonito/lmodroid_manifest.xml
+DEVICE_MANIFEST_FILE += device/google/bonito/lineage_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lmodroid/config/device_framework_matrix.xml
 
 # Partitions
